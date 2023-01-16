e906d475669b04df6372ad4537bf5231d862f6d7
mikkel agerlin <mikkel_ac@hotmail.com>
2023-01-16 17:52:02 +0100

    feat: rpd maps

--- src/components/MapList.tsx
+++ src/components/MapList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled, { css } from "styled-components";
 import RealmComponent from "./MapComponent";
 import { DBDMap } from "../types/map";
