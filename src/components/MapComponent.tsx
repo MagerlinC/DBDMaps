d5bcc7bae9c481ad48d2c4fdfb0bf6a4154f9931
mikkel agerlin <mikkel_ac@hotmail.com>
2025-01-26 18:31:45 +0100

    fix: layout for large realms

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -14,32 +14,56 @@ const RealmWrapper = styled.div<MapStyleProps>`
     gap: ${theme.spacing.medium};
     .map-entry {
       cursor: pointer;
+      white-space: nowrap;
       &:hover {
         text-decoration: underline;
       }
     }
   `}
 `;
+
+const MapListWrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: row;
+    gap: ${theme.spacing.medium};
+  `}
+`;
+
 type MapProps = {
   maps: DBDMap[];
   realmName: string;
   showMap: (map: DBDMap) => void;
 };
 const RealmComponent: React.FC<MapProps> = ({ maps, realmName, showMap }) => {
+  const [first5, rest] = [maps.slice(0, 5), maps.slice(5)];
   return (
     <RealmWrapper>
       <TextComponent variant={TextVariant.HEADER}>{realmName}</TextComponent>
-      <ul>
-        {maps.map((map) => (
-          <li
-            className={"map-entry"}
-            key={map.names[0]}
-            onClick={() => showMap(map)}
-          >
-            {map.names[0]}
-          </li>
-        ))}
-      </ul>
+      <MapListWrapper>
+        <ul>
+          {first5.map((map) => (
+            <li
+              className={"map-entry"}
+              key={map.names[0]}
+              onClick={() => showMap(map)}
+            >
+              {map.names[0]}
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {rest.map((map) => (
+            <li
+              className={"map-entry"}
+              key={map.names[0]}
+              onClick={() => showMap(map)}
+            >
+              {map.names[0]}
+            </li>
+          ))}
+        </ul>
+      </MapListWrapper>
     </RealmWrapper>
   );
 };
